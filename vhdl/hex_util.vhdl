69075871121b00795310c1c0de7169a1404aa395
makestuff <github.com@m3.ath.cx>
2013-04-04 11:35:42 +0100

    Fixed license headers

--- vhdl/hex_util.vhdl
+++ vhdl/hex_util.vhdl
@@ -1,5 +1,5 @@
 --
--- Copyright (C) 2009-2012 Chris McClelland
+-- Copyright (C) 2012 Chris McClelland
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Lesser General Public License as published by
